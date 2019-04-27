c1f684764a9cad65ff0bb9bfbac0af1a3ce3c07e
neall-dewaal <neall.dewaal@stratusolve.com>
2019-04-27 18:19:12 +0200

    Finalized full automation.

--- AzureFirstInstall.sh
+++ AzureFirstInstall.sh
@@ -25,15 +25,11 @@
 # DECLARE GLOBAL VARIABLES:
 # ===========================================================================================================
 # Directories
-usr=$USER
-DirectoryPermissionRewriteLocation=/var/www/html #Specify directory for which to rewrite permissions.
-OutputFileLocation=/home/$USER/Output.txt #specify your output file here.
-AllConf=/home/$USER/VMConfig.txt
 
 # Get system versioning
 phpCur=7.2
 mariadbCur=10.3
-isFirst=n
+isFirst=y
 
 # Colours
 Color_Off='\033[0m'       # Text Reset
@@ -44,7 +40,7 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 
 # ===========================================================================================================
-# Prints the variable passed into the indicated file //
+# Prints the variable passed into the indicated file 
 function printInput {
     strOutput=$(date '+%d/%m/%Y %H:%M:%S')
     strOutput="$strOutput $1"
@@ -53,9 +49,7 @@ function printInput {
 # Set the superuser's password //
 function setupUserPassword {
     echo -e "$Cyan \n Setting up Root Password... $Color_Off"
-    echo -e "$Yellow \n Enter the password that will be used for the root user: $Color_Off"
-    echo -e "$Yellow \n For Stratusolve this would be the same as the ssh password. $Color_Off"
-    sudo passwd
+    printf "$passwd\n$passwd" | sudo passwd
     printInput "Successfully updated Root password"
 }
 # Update packages and Upgrade system //
@@ -63,6 +57,7 @@ function UpdateSys {
     echo -e "$Cyan \n Updating System... $Color_Off"
     sudo ufw allow in "openSSH"
     sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
+    sudo set-timezone Africa/Johannesburg
     printInput "Successfully updated all packages, dependencies and distributions."
 }
 # Install Apache //
@@ -73,13 +68,14 @@ function InstallApache {
     sudo systemctl start apache2.service
     sudo systemctl enable apache2.service
     sudo ufw allow in "Apache Full"
-    sudo ufw -y enable
+    sudo ufw enable -y
     printInput "Successfully installed Apache2"
 }
 #Install MariaDb
 function InstallMaria {
     echo -e "$Cyan \n Installing MariaDb $Color_Off"
     sudo apt-get install software-properties-common -y
+    # here
     echo -e "$Yellow \n Adding keys $Color_Off"
     sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
     echo -e "$Yellow \n Adding repository $Color_Off"
@@ -87,6 +83,11 @@ function InstallMaria {
     echo -e "$Yellow \n Updating $Color_Off"
     sudo apt update -y
     echo -e "$Yellow \n Installing MariaDB $Color_Off"
+    sudo apt-get install debconf-utils -y
+    sudo export DEBIAN_FRONTEND=noninteractive
+    sudo export passwd
+    echo mariadb-server mysql-server/root_password password $passwd | sudo debconf-set-selections
+    echo mariadb-server mysql-server/root_password_again password $passwd | sudo debconf-set-selections
     sudo apt -y install mariadb-server mariadb-client
     sudo systemctl stop mariadb.service
     sudo systemctl start mariadb.service
@@ -96,14 +97,18 @@ function InstallMaria {
 #Clean and secure the database
 function WriteDatabasePermissions {
     echo -e "$Cyan \n Configuring Database $Color_Off"
-    mysql -u root -p$password<<-EOF
+    #UPDATE user SET password = PASSWORD($passwd) WHERE user = 'root';
+    mysql -u root -p$passwd<<-EOF
 use mysql;
 DELETE FROM user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+CREATE USER ${usr} IDENTIFIED BY '${passwd}';
 FLUSH PRIVILEGES;
 update user set plugin='mysql_native_password' where User='root';
+update user set plugin='mysql_native_password' where User='${usr}';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO '${usr}'@'%';
 FLUSH PRIVILEGES;
 EOF
     printInput "Successfully installed MariaDb"
@@ -145,18 +150,12 @@ function RestartServer {
 #Change permissions
 function WritePermissions {
     echo -e "$Cyan \n Changing default permissions of files $Color_Off"
-    sudo chmod -R 777 /etc/pam.d
-    sudo mkdir /etc/pam.d/custom-scripts
-    sudo mv /home/$username/chPermOwn.sh /etc/pam.d/custom-scripts
-    sudo cp /home/$username/Output.txt /etc/pam.d/custom-scripts/Output.txt
-    sudo mv /home/$username/stopBash.txt /etc/pam.d/custom-scripts
-    sudo chmod +x /etc/pam.d/custom-scripts/chPermOwn.sh
-    sudo rm /etc/pam.d/sshd
-    sudo mv /home/$username/sshd /etc/pam.d
-    sudo chmod -R 774 /etc/pam.d/custom-scripts
-
-    sudo adduser $username www-data
-    sudo chown -R $username:www-data /var/www/html
+    cd /etc/pam.d
+    sed -i '53i\session     optional    pam_exec.so quiet /etc/pam.d/custom-scripts/CheckFirstRun.sh\' sshd
+    #echo "session     optional    pam_exec.so quiet /etc/pam.d/custom-scripts/CheckFirstRun.sh" >> /etc/pam.d/sshd
+    #sudo chmod -R 774 /etc/pam.d/custom-scripts
+    sudo adduser $usr www-data
+    sudo chown -R www-data:www-data /var/www/html
     find /var/www/html -type d -exec chmod 755 {} +
     find /var/www/html -type f -exec chmod 775 {} +
     sudo chmod ug+s /var/www/html
@@ -167,26 +166,10 @@ function WritePermissions {
 ChangeDefaultDir () {
     echo -e "$Cyan \n Installing default landing directory $Color_Off"
     sudo su <<EOF
-killall -u $username; sleep 2; usermod -d /var/www/html $username &
+killall -u $usr; sleep 2; usermod -d /var/www/html $usr &
 EOF
     printInput "Successfully updated the default login directory."
 }
-
-#WELCOME PROMPT
-echo -e "$Green Hi! Welcome to the setup wizard! $Color_Off"
-echo -e "$Green Here we will set up your machine. $Color_Off"
-echo -e "$Green We will now present you with a list of questions to speed up the configuration. $Color_Off"
-echo -e "$Green Are you ready? [y/n]:"
-read -rsp $'Press any key to continue...' -n1 key
-echo -e "$Green Great! Here we go. $Color_Off"
-read -rsp "Is this the first time running this machine? [y/n]" -n1 key
-if [[ $key == y ]]; then
-    isFirst=y
-    echo -e "\n"
-fi
-read -p "What is the username used to SSH into the VM: " username
-read -p "What is the password used to SSH into the VM: " password
-
 #check versioning
 while read string
 do
@@ -199,18 +182,43 @@ do
     if [[ $string == *"mariadb"* ]]; then
         SUBSTR=$(echo $string | cut -d'=' -f 2)
     fi
-done < VMConfig.txt
+done < $AllConf
+function startupScript {
+    # Here we define the functions to run if it is the first time.
+    read -p "What is the username used to SSH into the VM: " username
+    read -p "What is the password used to SSH into the VM: " password
+    sudo chmod -R 777 /etc/pam.d
+    sudo mkdir /etc/pam.d/custom-scripts
+    sudo mv /home/$username/chPermOwn.sh /etc/pam.d/custom-scripts
+    sudo mv /home/$username/Output.txt /etc/pam.d/custom-scripts
+    sudo mv /home/$username/stopBash.txt /etc/pam.d/custom-scripts
+    sudo chmod +x /etc/pam.d/custom-scripts/chPermOwn.sh
+    sudo cp /home/$username/CheckFirstRun.sh /etc/pam.d/custom-scripts/CheckFirstRun.sh
+    sudo chmod +x /etc/pam.d/custom-scripts/CheckFirstRun.sh
+    sudo cp /home/$username/AzureFirstInstall.sh /etc/pam.d/custom-scripts/AzureFirstInstall.sh
+    sudo chmod +x /etc/pam.d/custom-scripts/AzureFirstInstall.sh
+    sudo mv /home/$username/FirstRun.txt /etc/pam.d/custom-scripts
+    usr=$username
+    passwd=$password
+}
 
-# Here we define the functions to run if it is the first time.
-if [[ $isFirst == y ]]; then
-    UpdateSys
-    setupUserPassword
-    InstallApache
-    InstallPHP
-    InstallMaria
-    InstallMyAdmin
-    RestartServer
-    WriteDatabasePermissions
-    WritePermissions
-    ChangeDefaultDir
-fi
+usr=$1
+passwd=$2
+#startupScript
+DirectoryPermissionRewriteLocation=/var/www/html #Specify directory for which to rewrite permissions.
+OutputFileLocation=/etc/pam.d/custom-scripts/Output.txt #specify your output file here.
+AllConf=/etc/pam.d/custom-scripts/VMConfig.txt
+StopBashFile=/etc/pam.d/custom-scripts/FirstRun.txt
+
+UpdateSys
+setupUserPassword
+InstallApache
+InstallPHP
+InstallMaria
+InstallMyAdmin
+RestartServer
+WriteDatabasePermissions
+WritePermissions
+RestartServer
+#ChangeDefaultDir
+
